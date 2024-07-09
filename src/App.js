d8b8a88e2fe223016f810bb90d4f4a1187ed413f
RAJAT <135589300+SKULLDUDEE@users.noreply.github.com>
2024-07-09 21:26:17 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import React from 'react'
+import Navbar from './components/navbar/Navbar'
+import News from './components/news/News'
+
+const App = () => {
+  return (
+    <>
+      <Navbar/>
+      <News/>
+    </>
+  )
+}
+
+export default App
